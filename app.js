b42f82e4502d3d0826e2e75c8085514e7b5bcd65
alfajoresditasel <alfajoresditasel@gmail.com>
2026-06-28 20:42:54 -0500

    Conexión con Firebase

--- app.js
+++ app.js
@@ -1,41 +1,68 @@
-//==================================
-// TEMPERATURA
-//==================================
+//======================================
+// CONFIGURACIÓN FIREBASE
+//======================================
 
-let temperatura = 0;
+const firebaseConfig =
+{
+    apiKey: "AIzaSyDKrY0YFxSpzsy5gYKFSwTz60AA2zmMTBE",
+    authDomain: "temperatura-esp32-bd68c.firebaseapp.com",
+    databaseURL: "https://temperatura-esp32-bd68c-default-rtdb.firebaseio.com",
+    projectId: "temperatura-esp32-bd68c",
+    storageBucket: "temperatura-esp32-bd68c.firebasestorage.app",
+    messagingSenderId: "941827526197",
+    appId: "1:941827526197:web:cd61466118474ecc6beb7c"
+};
 
-//==================================
+//======================================
+// INICIALIZAR FIREBASE
+//======================================
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+//======================================
+// ACTUALIZAR TERMÓMETRO
+//======================================
 
 function actualizarTermometro(temp)
 {
 
-    if(temp<0)
-        temp=0;
+    if(temp < 0)
+        temp = 0;
 
-    if(temp>150)
-        temp=150;
+    if(temp > 150)
+        temp = 150;
 
     document.getElementById("temperatura").innerHTML =
-    temp.toFixed(1)+" °C";
+    temp.toFixed(1) + " °C";
 
-    let porcentaje=(temp/150)*100;
+    let porcentaje = (temp / 150) * 100;
 
-    document.getElementById("barra").style.height=
-    porcentaje+"%";
+    document.getElementById("barra").style.height =
+    porcentaje + "%";
 
 }
 
-//==================================
+//======================================
+// ESPERANDO DATOS
+//======================================
 
-// SOLO PARA PRUEBAS
+document.getElementById("temperatura").innerHTML =
+"-- °C";
 
-setInterval(function(){
+//======================================
+// LEER TEMPERATURA DE FIREBASE
+//======================================
 
-    temperatura++;
+database.ref("temperatura").on("value", function(snapshot)
+{
 
-    if(temperatura>150)
-        temperatura=0;
+    let temp = snapshot.val();
 
-    actualizarTermometro(temperatura);
+    if(temp != null)
+    {
+        actualizarTermometro(parseFloat(temp));
+    }
 
-},500);
+});
